b413662f6631f29c3612eaec690f1d907595bdce
Anton Maisak <anton@maisak.ru>
2022-01-12 18:17:31 +0300

    Added ServiceWorker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ngx-toui-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
 }
